daca13b9dd67917f233a0c7c68479cf64dc1a170
DiamondCat1234567 <240060202+DaimondCat1234567@users.noreply.github.com>
2026-03-13 11:45:55 +0300

    Fix the word service

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
 				<p class='footer-text'>
 				    <a href='/BluOf/credits.html'>CREDITS</a>|
 					<a href='/BluOf/about.html'>ABOUT</a>|
-					<a href='/BluOf/ToS.html'>Terms Of Servise and Use</a>
+					<a href='/BluOf/ToS.html'>Terms Of Service and Use</a>
 				</p>
 				<p class="footer-text">Version: 0.0.1 Alpha</p>
             </div>
